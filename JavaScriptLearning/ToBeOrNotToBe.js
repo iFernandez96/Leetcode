44110566089a2c646e1e2f59b2f4288788cc38e5
Israel Fernandez <israelfernandez96@gmail.com>
2025-01-12 23:16:06 -0800

    first Pass ToBeOrNotToBe

--- JavaScriptLearning/ToBeOrNotToBe.js
+++ JavaScriptLearning/ToBeOrNotToBe.js
@@ -3,7 +3,20 @@
  * @return {Object}
  */
 let expect = (val) => {
-    
+    return () => {
+        toBe: (val2) => {
+            if (val !== val2) {
+                throw new Error("Not Equal");
+            }
+            return true;
+        };
+        notToBe: (val3) => {
+            if (val === val3) {
+                throw new Error("Equal");
+            }
+            return true;
+        };
+    };
 };
 
 /**
